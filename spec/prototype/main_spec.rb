553135f540b1f1429f4eef04f5f28eaf1e6aac67
naokikotani <nok.naoki.tigers.415@icloud.com>
2025-11-16 19:36:57 +0900

    第6章-テスト実装

---
+++ spec/prototype/main_spec.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+
+RSpec.describe Manager do
+  describe 'Prototype pattern' do
+    let(:manager) { Manager.new }
+
+    before do
+      upen = UnderlinePen.new('-')
+      mbox = MessageBox.new('*')
+      sbox = MessageBox.new('/')
+
+      manager.register('strong message', upen)
+      manager.register('warning box', mbox)
+      manager.register('slash box', sbox)
+    end
+
+    it do
+      p1 = manager.create('strong message')
+      expect { p1.use('Hello, world.') }.to output(<<~TEXT).to_stdout
+        Hello, world.
+        -------------
+      TEXT
+    end
+
+    it do
+      p2 = manager.create('warning box')
+      expect { p2.use('Hello, world.') }.to output(<<~TEXT).to_stdout
+        ****************
+        *Hello, world.*
+        ****************
+      TEXT
+    end
+
+    it do
+      p3 = manager.create('slash box')
+      expect { p3.use('Hello, world.') }.to output(<<~TEXT).to_stdout
+        ////////////////
+        /Hello, world./
+        ////////////////
+      TEXT
+    end
+  end
+end
