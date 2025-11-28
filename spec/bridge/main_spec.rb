71694dfbfb2cc3f916910f8ce14a29819f7a276a
naokikotani <nok.naoki.tigers.415@icloud.com>
2025-11-28 10:10:26 +0900

    第9章-演習問題

--- spec/bridge/main_spec.rb
+++ spec/bridge/main_spec.rb
@@ -46,4 +46,119 @@ RSpec.describe 'Bridge Pattern Display' do
       +----------------+
     EOS
   end
+
+  describe 'RandomDisplay' do
+    it 'random_display displays 0 to times-1 lines randomly' do
+      srand(42) # rand(5) returns 3 with this seed
+      d = RandomDisplay.new(StringDisplayImpl.new("Test"))
+      d.random_display(5)
+      expect(stdout.string).to eq <<~EOS
+        +----+
+        |Test|
+        |Test|
+        |Test|
+        +----+
+      EOS
+    end
+
+    it 'random_display can also display zero times' do
+      srand(2) # rand(5) returns 0 with this seed
+      d = RandomDisplay.new(StringDisplayImpl.new("Test"))
+      d.random_display(5)
+      expect(stdout.string).to eq <<~EOS
+        +----+
+        +----+
+      EOS
+    end
+  end
+
+  describe 'FileDisplayImpl' do
+    let(:test_file) { '/tmp/bridge_test_file.txt' }
+
+    before do
+      File.write(test_file, "Hello\nWorld\nRuby")
+    end
+
+    after do
+      File.delete(test_file) if File.exist?(test_file)
+    end
+
+    it 'displays file contents with border' do
+      d = Display.new(FileDisplayImpl.new(test_file))
+      d.display
+
+      expect(stdout.string).to eq <<~EOS
+        +-----+
+        |Hello|
+        |World|
+        |Ruby |
+        +-----+
+      EOS
+    end
+
+    it 'works with CountDisplay for multi_display' do
+      d = CountDisplay.new(FileDisplayImpl.new(test_file))
+      d.multi_display(2)
+
+      expect(stdout.string).to eq <<~EOS
+        +-----+
+        |Hello|
+        |World|
+        |Ruby |
+        |Hello|
+        |World|
+        |Ruby |
+        +-----+
+      EOS
+    end
+
+    it 'handles non-existent file' do
+      d = Display.new(FileDisplayImpl.new('/tmp/non_existent_file.txt'))
+      d.display
+
+      expect(stdout.string).to include('File not found')
+    end
+  end
+
+  describe 'IncreasingDisplayImpl' do
+    it 'displays pattern like Fig.9-4 (<, *, >)' do
+      d = Display.new(IncreasingDisplayImpl.new('<', '*', '>', 4))
+      d.display
+
+      expect(stdout.string).to eq <<~EOS
+        <>
+        <*>
+        <**>
+        <***>
+      EOS
+    end
+
+    it 'displays pattern like Fig.9-5 (|, ##, -)' do
+      d = Display.new(IncreasingDisplayImpl.new('|', '##', '-', 6))
+      d.display
+
+      expect(stdout.string).to eq <<~EOS
+        |-
+        |##-
+        |####-
+        |######-
+        |########-
+        |##########-
+      EOS
+    end
+
+    it 'works with CountDisplay for multi_display' do
+      d = CountDisplay.new(IncreasingDisplayImpl.new('[', '+', ']', 3))
+      d.multi_display(2)
+
+      expect(stdout.string).to eq <<~EOS
+        []
+        [+]
+        [++]
+        []
+        [+]
+        [++]
+      EOS
+    end
+  end
 end
